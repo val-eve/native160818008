d8581e69928e0bf05a2c8f9521624cfd2016f98f
val-eve <s160818008@student.ubaya.ac.id>
2021-08-18 14:47:39 +0700

    toast added

--- app/src/main/java/com/advnmpproj/native160818008/MainActivity.kt
+++ app/src/main/java/com/advnmpproj/native160818008/MainActivity.kt
@@ -2,10 +2,13 @@ package com.advnmpproj.native160818008
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Welcome to Android :)", Toast.LENGTH_LONG).show()
     }
 }
